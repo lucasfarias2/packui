7cf808d07bcd0f0e74db0c83be87588d27bee4eb
Lucas Farias <lukf95@gmail.com>
2022-12-13 23:01:31 +0100

    Styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <title>PackUI</title>
       </Head>
 
-      <nav className="h-[40px] shadow flex items-center p-6 relative">
+      <nav className="h-[50px] shadow flex items-center px-6 relative bg-black text-white">
         <div className="font-semibold text-xl logo">PackUI</div>
       </nav>
 
@@ -16,10 +16,11 @@ export default function Home() {
         <div className="bg-neutral-50 w-[270px] p-6">
           <div className="mb-4 text-neutral-500 uppercase text-sm">Components</div>
           <ul>
-            <li className="hover:bg-neutral-100 rounded-lg py-2 px-4 cursor-pointer">Card</li>
+            <li className="hover:bg-neutral-100 rounded-lg py-2 px-2 cursor-pointer">- Card</li>
           </ul>
         </div>
-        <div className="bg-neutral-100 flex-1 p-4">
+        <div className="bg-neutral-100 flex-1 p-6">
+          <h1 className="text-3xl font-semibold mb-4">Card</h1>
           <Card>Card test</Card>
         </div>
       </main>
