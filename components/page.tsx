100028230a7e4e68bfc637ae40383969d4c02a03
Lucas Farias <lukf95@gmail.com>
2022-12-15 00:22:21 +0100

    Media pge

--- components/page.tsx
+++ components/page.tsx
@@ -11,7 +11,7 @@ const Page = ({ children, className }: IProps) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav className="h-[50px] shadow-md flex items-center px-6 relative bg-neutral-800 text-white">
+      <nav className="h-[50px] shadow flex items-center px-6 relative bg-neutral-800 text-white z-10">
         <Link href="/" className="font-semibold text-xl logo mr-4">
           PackUI
         </Link>
@@ -24,7 +24,7 @@ const Page = ({ children, className }: IProps) => {
       </nav>
 
       <main className={`flex flex-row ${className}`}>
-        <div className="bg-neutral-50 w-[270px] py-4">
+        <div className="bg-neutral-50 w-[270px] py-4 shadow relative h-[100vh]">
           <MenuItem href="/" label="Home" />
           <div className="text-neutral-500 uppercase text-sm p-4">Components</div>
           <MenuItem href="/components/card" label="Card" />
---
+++ pages/components/media.tsx
@@ -0,0 +1,11 @@
+import Page from 'components/page';
+
+const MediaPage = () => {
+  return (
+    <Page>
+      <h1 className="text-3xl font-semibold mb-4">Media</h1>
+    </Page>
+  );
+};
+
+export default MediaPage;
